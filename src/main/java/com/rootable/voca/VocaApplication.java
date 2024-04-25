c776d6a2062a9eba6a5ef18a5a7e51dd391b5b6c
wndudrla1011 <wndudrla1011@naver.com>
2024-04-25 20:29:22 +0900

    Initial commit

---
+++ src/main/java/com/rootable/voca/VocaApplication.java
@@ -0,0 +1,13 @@
+package com.rootable.voca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rootable/voca/VocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootable.voca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
