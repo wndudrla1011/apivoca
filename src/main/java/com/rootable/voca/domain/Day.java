9c4fe6e4687262b8b42c08bfddc12dce50678281
wndudrla1011 <wndudrla1011@naver.com>
2024-04-26 17:38:48 +0900

    added Builder

--- src/main/java/com/rootable/voca/domain/Day.java
+++ src/main/java/com/rootable/voca/domain/Day.java
@@ -1,11 +1,15 @@
 package com.rootable.voca.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @SequenceGenerator(
         name = "DAY_SEQ_GENERATOR",
