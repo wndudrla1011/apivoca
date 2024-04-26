3b2980d3d0796a8f337cfb0fe5dc74ea95e2b77d
wndudrla1011 <wndudrla1011@naver.com>
2024-04-26 16:32:16 +0900

    created DayRepository

---
+++ src/main/java/com/rootable/voca/repository/DayRepository.java
@@ -0,0 +1,7 @@
+package com.rootable.voca.repository;
+
+import com.rootable.voca.domain.Day;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DayRepository extends JpaRepository<Long, Day> {
+}
